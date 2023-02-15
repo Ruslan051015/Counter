df049a24860cff6e102177490d7730d4c6983e16
Ruslan051015 <ruslaniosdev@gmail.com>
2023-02-15 17:45:55 +0400

    Добавлены значок человека, лейбл текста и кнопка, на которую повешено действие по увеличению счётчика и смене иконки человека

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var isHandsRaised = false
+    private var counter = 0
+    
+    @IBOutlet weak var figureImageView: UIImageView!
+    @IBOutlet weak var incrementCounterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        figureImageView.image = UIImage(systemName: "figure.mixed.cardio")
+        counterLabel.text = "Значение счётчика: 0"
+       
     }
 
-
+    @IBAction func incrementCounter(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+        if isHandsRaised {
+            figureImageView.image = UIImage(systemName: "figure.mixed.cardio")
+        } else {
+            figureImageView.image = UIImage(systemName: "figure.arms.open")
+        }
+        isHandsRaised.toggle()
+        
+    }
+    
 }
 
